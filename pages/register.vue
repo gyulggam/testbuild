bbbb79d6b8c0392c432e589f904b74434d266806
KIMJINSU <45416779+wdamanghara@users.noreply.github.com>
2021-12-09 10:31:54 +0900

    Update register.vue

--- pages/register.vue
+++ pages/register.vue
@@ -1,102 +1,174 @@
 <template>
     <div class="d-flex-center h-100">
-        <div class="login-page d-flex-column">
-            <div class="login-header ptb-20">
+        <div class="register-page d-flex-column">
+             <div class="register-header ptb-20">
                 <div>
                     <img src="~/assets/images/logo_MachLake_Nomark.png">
                 </div>
             </div>
-
-            <div class="login-content d-flex-spacebetween ml-10">
-                <div class="login-page-img-wrap">
+            <div class="register-content d-flex-spacebetween">
+                <div class="register-page-img-wrap">
+                    <p class="register-page-title mb-40">Sign up</p>
                     <img
+                        class="register-mainimg"
                         src="~/assets/images/login/login_image.png"
                     />
                 </div>
 
-                <div class="login-page-form-wrap d-flex-column">
-                    <p class="login-page-title text-center mb-40">
-                        Log in
-                    </p>
-
+                <form
+                    class="register-page-form-wrap d-flex-spacebetween"
+                    @submit.prevent="confirmSingup"
+                >
                     <div
-                        class="google-login-btn semi-title d-flex-center"
-                        @click= "handelClickGoogleLogin"
+                        class="register-page-required d-flex-column"
                     >
-                        <img src="~/assets/images/login/logo_google.png" class="mr-10">
-                        Continue with Google
+                        <p class="havy-title mb-40">Required</p>
+
+                        <input
+                            class="section-card-input w-100 mb-40"
+                            type="text"
+                            v-model="sRegisterForm.username"
+                            placeholder="Id"
+                        />
+
+                        <input
+                            class="section-card-input w-100 mb-10"
+                            type="password"
+                            v-model="sRegisterForm.password"
+                            placeholder="Password"
+                        />
+                        <input
+                            class="section-card-input w-100 mb-60"
+                            type="password"
+                            v-model="sConfirmPassword"
+                            placeholder="Confirm Password"
+                        />
+
+                        <input
+                            class="section-card-input w-100 mb-10"
+                            type="email"
+                            v-model="sRegisterForm.email"
+                            placeholder="Email"
+                        />
+
+                        <input
+                            v-if="sIsClickEmailConfirmBtn"
+                            v-model="sConfirmForm.code"
+                            class="section-card-input w-100 mb-10"
+                            placeholder="Code"
+                        />
+
+                        <!-- <button
+                            v-if="sIsClickEmailConfirmBtn"
+                            class="confirm-btn semi-title"
+                            @click="confirmSingup"
+                        >
+                            Verify Email
+                        </button> -->
+
+                        <div
+                            v-if="!sIsClickEmailConfirmBtn"
+                            class="confirm-btn semi-title"
+                            @click="checkSignup"
+                        >
+                            Submit Code
+                        </div>
+
+                        <button
+                            v-else
+                            type="submit"
+                            class="register-btn main-title"
+                        >
+                            Sign up
+                        </button>
+
                     </div>
 
-                    <form
-                        class="login-form pt-40 mt-20"
-                        @submit.prevent="handelClickLogin"
+                    <!-- <div
+                        class="register-page-optional d-flex-column"
                     >
+                        <p class="havy-title mb-40">Optional</p>
+
+                        <span class="semi-title mb-10">Name</span>
                         <input
-                            type="text"
-                            placeholder="ID"
-                            v-model="sLoginInfo.username"
                             class="section-card-input w-100 mb-10"
+                            type="text"
+                            v-model="sRegisterForm.firstname"
+                            placeholder="First name"
                         />
-
                         <input
-                            type="password"
-                            placeholder="Password"
-                            v-model="sLoginInfo.password"
-                            class="section-card-input w-100"
+                            class="section-card-input w-100 mb-40"
+                            type="text"
+                            v-model="sRegisterForm.lastname"
+                            placeholder="Last name"
                         />
 
-                        <button type="submit" class="login-btn main-title">Login</button>
-                    </form>
-
-                    <nuxt-link to="/register" class="login-link semi-title">forgot password?</nuxt-link>
-                    <nuxt-link to="/register" class="login-link semi-title">No Account yet? <span class="text-primary-color">Sign up</span></nuxt-link>
-                </div>
+                        <span class="semi-title mb-10">Phone number</span>
+                        <MazPhoneNumberInput
+                            id="phone-number-selecter"
+                            size="sm"
+                            no-example
+                            v-model="sRegisterForm.phonenumber"
+                            :translations="sTranslations"
+                            @update="sPhonnumberInfo = $event"
+                        />
+                        <button type="submit" class="register-btn main-title">Sign up</button>
+                    </div> -->
+                </form>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex';
-
 export default {
-    data: () => ({
-        sLoginInfo: {
-            username: '',
-            password: ''
+    data() {
+        return {
+            sIsClickEmailConfirmBtn: false,
+            sStep:"register",
+            sPhonnumberInfo: null,
+            sTranslations: {
+                countrySelectorLabel: null,
+            },
+            sConfirmPassword: "",
+            sRegisterForm: {
+                username: "",
+                email: "",
+                password: "",
+            },
+            sConfirmForm: {
+                username: "",
+                code: ""
+            }
         }
-    }),
-    methods: {
-        ...mapActions('auth', [
-            "login",
-            "logout",
-            "googleLogin"
-        ]),
-        async handelClickLogin() {
-            try {
-                await this.login(this.sLoginInfo);
-            } catch (error) {
-                console.log({ error });
-            }
-        },
-        async handelClickLogout() {
-            try {
-                await this.logout();
-            } catch (error) {
-                console.log({ error });
-            }
-        },
-        async handelClickGoogleLogin() {
-            try {
-                this.googleLogin();
-            } catch (error) {
-                console.log({ error });
-            }
-        },
     },
+
+    methods: {
+        async checkSignup() {
+            try {
+                const sCheckSignupResult = await this.$store.dispatch('auth/register', this.sRegisterForm);
+
+                if(sCheckSignupResult) {
+                    this.sIsClickEmailConfirmBtn = true;
+                    this.sConfirmForm.username = this.sRegisterForm.username
+                }
+            } catch (aErr) {
+                this.$toasted.error(aErr);
+            }
+        },
+        async confirmSingup() {
+            try {
+                await this.$store.dispatch('auth/confirmRegistration', this.sConfirmForm)
+                await this.$store.dispatch('auth/login', this.sRegisterForm)
+                this.$router.push('/lake')
+            } catch (aErr) {
+                this.$toasted.error(aErr);
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "~/assets/scss/page/_login.scss";
+@import "~/assets/scss/page/_register.scss";
 </style>
